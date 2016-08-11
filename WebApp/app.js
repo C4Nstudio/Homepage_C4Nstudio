44a77e296fb3f6c7a6c0513ba585ca99ce11ae1a
C4Nstudio <C4Nstudio@foxmail.com>
2016-08-11 20:15:28 +0800

    Install postcss-middleware.

--- WebApp/app.js
+++ WebApp/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -22,6 +23,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'compressed'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRoute);
--- WebApp/components/Footer.js
+++ WebApp/components/Footer.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Footer = React.createClass({
   render: function() {
     return (
-      <footer id="footer" className='text-center'>
+      <footer className='text-center'>
         <div>
           <small>Copyright&nbsp;&copy;&nbsp;<a href='http://C4Nstudio.com'>C4Nstudio.com</a> . All Rights Reserved.</small>
         </div>
