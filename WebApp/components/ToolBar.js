e72ba62d27a4d8040e739a717e234fcbd678707c
C4Nstudio <C4Nstudio@foxmail.com>
2016-08-20 20:21:36 +0800

    Update ToolBar.js.
    ShouldComponentUpdate: false

--- WebApp/components/ToolBar.js
+++ WebApp/components/ToolBar.js
@@ -1,6 +1,9 @@
 var React = require('react');
 
 var ToolBar = React.createClass({
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return false;
+  },
   render: function() {
     return (
       <div id='tool-bar' className="hide-for-small-only">
