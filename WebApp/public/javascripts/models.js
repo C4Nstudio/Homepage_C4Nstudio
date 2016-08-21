46459cc0c2fe6f106d7b03fb6cc20be37355ae1c
C4Nstudio <C4Nstudio@foxmail.com>
2016-08-21 21:15:51 +0800

    Add mongoose models & dbHandle.

---
+++ WebApp/public/javascripts/models.js
@@ -0,0 +1,9 @@
+module.exports = {
+  post : {
+    blogTitle : { type : String , required : true },
+    blogCreatedTime : { type : String, required : true},
+    blogCategory : { type : String, required : true },
+    blogDescription : { type : String, required : true},
+    blogBody : { type : String, required : true}
+  }
+}
