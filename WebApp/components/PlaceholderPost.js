ebb724e772d6315f419bbd7d5fd079373b8a7d49
C4Nstudio <C4Nstudio@foxmail.com>
2016-08-29 08:34:03 +0800

    Update PlaceHolderPost.js.
    Improve performance.

--- WebApp/components/PlaceholderPost.js
+++ WebApp/components/PlaceholderPost.js
@@ -1,6 +1,9 @@
 var React = require('react');
 
 var PlaceholderPost = React.createClass({
+  shouldComponentUpdate: function() {
+    return false;
+  },
   render: function() {
     return (
       <div className="post-placeholder">
